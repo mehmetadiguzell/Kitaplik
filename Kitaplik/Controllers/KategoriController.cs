c47dd150ff07b705dbbec3456e7de94eb09e11b4
mehmetadiguzell <76685004+mehmetadiguzell@users.noreply.github.com>
2022-05-19 18:20:03 +0300

    ASP.NET MVC

---
+++ Kitaplik/Controllers/KategoriController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kitaplik.Models;
+
+namespace Kitaplik.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        KitaplikDbEntities db = new KitaplikDbEntities();
+        public ActionResult Index()
+        {
+            var model = db.Kategori.ToList();
+            return View(model);
+        }
+
+        public ActionResult Ekle()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ekle(Kategori kategori)
+        {
+            db.Kategori.Add(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Guncelle(int id)
+        {
+            var model=db.Kategori.Find(id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Guncelle(Kategori kategori)
+        {
+            var model = db.Kategori.FirstOrDefault(X=>X.Id==kategori.Id);
+            model.KategoriAd = kategori.KategoriAd;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Kitaplik/Controllers/KitapController.cs
@@ -0,0 +1,74 @@
+﻿using Kitaplik.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Kitaplik.Controllers
+{
+    public class KitapController : Controller
+    {
+        // GET: Kitap
+        KitaplikDbEntities db = new KitaplikDbEntities();
+        public ActionResult Index()
+        {
+            var model = db.Kitap.ToList();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Ekle(Kitap kitap)
+        {
+            db.Kitap.Add(kitap);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Ekle()
+        {
+            List<SelectListItem> selectLists = (from i in db.Kategori
+                                                select new SelectListItem()
+                                                {
+                                                    Text = i.KategoriAd,
+                                                    Value = i.Id.ToString()
+                                                }).ToList();
+            ViewBag.list= selectLists;
+
+            return View();
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var model = db.Kitap.FirstOrDefault(x => x.Id == id);
+            db.Kitap.Remove(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Guncelle(int id)
+        {
+            List<SelectListItem> selectLists = (from i in db.Kategori
+                                                select new SelectListItem()
+                                                {
+                                                    Text = i.KategoriAd,
+                                                    Value = i.Id.ToString()
+                                                }).ToList();
+            ViewBag.list = selectLists;
+
+            var model = db.Kitap.FirstOrDefault(x => x.Id == id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Guncelle(Kitap kitap)
+        {
+            var model = db.Kitap.Find(kitap.Id);
+            model.KitapAd = kitap.KitapAd;
+            model.Yazari = kitap.Yazari;
+            model.SayfaSayisi = kitap.SayfaSayisi;
+            model.KategoriId = kitap.KategoriId;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
